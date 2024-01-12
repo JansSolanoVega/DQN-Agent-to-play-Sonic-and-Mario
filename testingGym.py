692efe18b95b9f096e743654f696184a2b075045
JansSolanoVega <janssv.200604@gmail.com>
2024-01-12 19:49:23 +0100

    Adding game to retro

---
+++ testingGym.py
@@ -0,0 +1,36 @@
+import retro
+
+env = retro.make("SonicTheHedgehog-Genesis", "LabyrinthZone.Act1")
+
+env.reset()
+
+done = False
+
+possible_actions = {
+    # No Operation
+    0: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    # Left
+    1: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    # Right
+    2: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    # Left, Down
+    3: [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    # Right, Down
+    4: [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    # Down
+    5: [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    # Down, B
+    6: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    # B
+    7: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+}
+
+while not done:
+    env.render()
+
+    action = possible_actions[0]
+
+    ob, rew, done, info = env.step(action)
+    
+
+    
