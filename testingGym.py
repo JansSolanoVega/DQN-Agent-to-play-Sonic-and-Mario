fa89c0bc4c7267eb7c866cc7d0c212690c64bf45
JansSolanoVega <janssv.200604@gmail.com>
2024-01-12 20:57:48 +0100

    Possible actions

--- testingGym.py
+++ testingGym.py
@@ -1,4 +1,6 @@
 import retro
+from utils import *
+import numpy as np
 
 env = retro.make(game="SonicTheHedgehog-Genesis", state="LabyrinthZone.Act1")
 
@@ -6,31 +8,13 @@ env.reset()
 
 done = False
 
-possible_actions = {
-    # No Operation
-    0: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    # Left
-    1: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    # Right
-    2: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    # Left, Down
-    3: [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-    # Right, Down
-    4: [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-    # Down
-    5: [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    # Down, B
-    6: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    # B
-    7: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-}
-
 while not done:
     env.render()
 
-    action = possible_actions[0]
+    action = POSSIBLE_ACTIONS[np.random.randint(0, len(POSSIBLE_ACTIONS))]
 
     ob, rew, done, info = env.step(action)
+
     
 
     
