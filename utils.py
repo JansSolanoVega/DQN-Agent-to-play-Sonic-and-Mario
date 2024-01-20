c7b09077f1c64ed7173fef595499c0654deabf0c
JansSolanoVega <janssv.200604@gmail.com>
2024-01-20 03:06:32 +0100

    Adding wrappers apply function

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from gym.wrappers import FrameStack
+from wrappers import *
+
 POSSIBLE_ACTIONS = {
     # No Operation
     0: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -18,3 +21,10 @@ POSSIBLE_ACTIONS = {
 }
 
 MAP_IDS_TO_NAME = ["None", "Left", "Right", "Left, Down", "Right, Down", "Down", "Down, B", "B"]
+
+def apply_wrappers(env, skip=4, shape=(156, 156), num_stack=4):
+    env = SkipFrame(env, skip=skip)
+    env = GrayScaleObservation(env)
+    env = ResizeObservation(env, shape=shape)
+    env = FrameStack(env, num_stack=num_stack)
+    return env
