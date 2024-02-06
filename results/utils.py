1dfb65770aa49d1459acd45528c1ff53ab629c08
JansSolanoVega <janssv.200604@gmail.com>
2024-02-07 00:40:20 +0100

    replay best model and ploting results

--- results/utils.py
+++ results/utils.py
@@ -43,19 +43,19 @@ def plot_average_reward(data, title="Episodes trained vs. Average Rewards", n_av
             np.convolve(data, np.ones((n_average,))/n_average, mode="valid").tolist())
     plt.show()
         
-def plot_error_lineplot(list_info, n_average, xlabel="episode_group_av", ylabel="rewards"):
-  x = np.asarray(list_info)
-  df = pd.DataFrame(list(zip(range(0, len(x)), x)), columns=['episode', 'info'])
-  new_df = pd.DataFrame()
-  subdf_n_average = []#n episodes will be one item here
-  for i in range(len(x)-n_average+1):
-    #print(df[i: i+n_average]["reward"].values)
-    data = {xlabel: np.ones(n_average)*i,
-            ylabel: df[i: i+n_average]["info"].values}
-    sub_df = pd.DataFrame(data)
-    subdf_n_average.append(sub_df)
-  new_df = pd.concat(subdf_n_average, ignore_index=True)
+# def plot_error_lineplot(list_info, n_average, xlabel="episode_group_av", ylabel="rewards"):
+#   x = np.asarray(list_info)
+#   df = pd.DataFrame(list(zip(range(0, len(x)), x)), columns=['episode', 'info'])
+#   new_df = pd.DataFrame()
+#   subdf_n_average = []#n episodes will be one item here
+#   for i in range(len(x)-n_average+1):
+#     #print(df[i: i+n_average]["reward"].values)
+#     data = {xlabel: np.ones(n_average)*i,
+#             ylabel: df[i: i+n_average]["info"].values}
+#     sub_df = pd.DataFrame(data)
+#     subdf_n_average.append(sub_df)
+#   new_df = pd.concat(subdf_n_average, ignore_index=True)
 
-  #Plot
-  sns.set(style="darkgrid")
-  sns.lineplot(x="episode_group_av", y="rewards", data=new_df, ci="sd")
+#   #Plot
+#   sns.set(style="darkgrid")
+#   sns.lineplot(x="episode_group_av", y="rewards", data=new_df, ci="sd")
